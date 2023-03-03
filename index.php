0c153beafd561c13440e77519f1354546e6af114
tomaspa21 <palaciostomas73@gmail.com>
2023-03-03 14:57:34 -0300

    imagenes_renombrar

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manejo de imagenes</title>
+</head>
+<body>
+    <h2> Subir imagen</h2>
+    <form action="subir.php" method="POST" enctype="multipart/form-data">
+    <input type="file" name="archivo">
+    <br>
+    <br>
+        <input type="submit">
+
+    </form>
+</body>
+</html>
